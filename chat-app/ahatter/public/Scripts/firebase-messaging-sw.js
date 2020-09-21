ad1fc916081be4a5b248d6e2d3d2a1c299344e84
MaarijAhmed <65016504+MaarijAhmed@users.noreply.github.com>
2020-09-21 08:33:25 +0500

    Add files via upload

---
+++ chat-app/ahatter/public/Scripts/firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+﻿// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-auth.js');
+
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBrAYeOc_KpmPzG0mbZTjeV4dGQ_FT5FzE",
+    authDomain: "chatapp-8481a.firebaseapp.com",
+    databaseURL: "https://chatapp-8481a.firebaseio.com",
+    projectId: "chatapp-8481a",
+    storageBucket: "chatapp-8481a.appspot.com",
+    messagingSenderId: "1042554257937",
+    appId: "1:1042554257937:web:8cb882513fce88f9a727b0",
+    measurementId: "G-0ST7DGMG1Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'You have new message';
+    const notificationOptions = {
+        body: payload.data.message,
+        icon: payload.data.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+
+
---
+++ chat-app/ahatter/public/Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBrAYeOc_KpmPzG0mbZTjeV4dGQ_FT5FzE",
+    authDomain: "chatapp-8481a.firebaseapp.com",
+    databaseURL: "https://chatapp-8481a.firebaseio.com",
+    projectId: "chatapp-8481a",
+    storageBucket: "chatapp-8481a.appspot.com",
+    messagingSenderId: "1042554257937",
+    appId: "1:1042554257937:web:8cb882513fce88f9a727b0",
+    measurementId: "G-0ST7DGMG1Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
